9418bc5f28e2d7eab0781bb688336cb9c9d6e650
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-06 12:27:30 -0700

    [Studio] Modified screen "powewowsp2"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -2,5 +2,5 @@ angular.module('app').controller('app', app);
 function app($rootScope,$scope, app) {
     'use strict';
     
-    $rootScope.bMenuVisible = false;
+    $rootScope.bMenuVisible = tri;
 }
