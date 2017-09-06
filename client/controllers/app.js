f2cd7df973ec580f1d73e6b424b5944e240015c0
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-06 12:35:40 -0700

    [Studio] Modified screen "powewowsp2"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -3,15 +3,16 @@ function app($rootScope,$scope, app, $timeout) {
     'use strict';
     
     $rootScope.bMenuVisible = false;
-     var timer = $timeout(function () {
+    /*
+    var timer = $timeout(function () {
         console.warn("bMenuVisible="+$rootScope.bMenuVisible);
          $rootScope.bMenuVisible = true; 
          console.warn("bMenuVisible="+$rootScope.bMenuVisible);
     }, 5000);
-    
+    */
     $rootScope.fEnableMenu = function(bValue){
          $scope.bMenuVisible = bValue;
-         console.warn("fEnableMenu bMenuVisible="+$scope.bMenuVisible)
+         console.warn("fEnableMenu bMenuVisible="+$scope.bMenuVisible);
     }
     
 }
