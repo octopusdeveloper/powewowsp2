7aae403e5f31cc6dd84a9d2c79d039242b4b66c1
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-06 08:30:50 -0700

    [Studio] Modified screen "powewowsp2", Modified screen "vLogin"

--- client/controllers/app/vlogin.js
+++ client/controllers/app/vlogin.js
@@ -14,6 +14,7 @@ function app_vlogin($scope, app, $timeout) {
         $scope.bLoading=true;
         var timer = $timeout(function () {
            $scope.bLoading=false;
+           app.go('app.vIssueadd')
         }, 2000);
     }     
     
