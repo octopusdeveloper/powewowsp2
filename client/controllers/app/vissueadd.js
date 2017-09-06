c6183c77214dbbe98e449e707d07f03079d27dae
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-06 12:06:35 -0700

    [Studio] Modified screen "vIssueadd"

--- client/controllers/app/vissueadd.js
+++ client/controllers/app/vissueadd.js
@@ -2,7 +2,7 @@ angular.module('app').controller('app_vissueadd', app_vissueadd);
 function app_vissueadd($scope, app,$ionicSideMenuDelegate) {
     'use strict';
     app.init($scope);
-    //console.log("entro!");
+    console.log("entro!");
     $ionicSideMenuDelegate.canDragContent(true);
     //$scope.bMenuVisible = true;
 }
