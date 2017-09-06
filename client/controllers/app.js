df2f9bd66653e7a9e392b0d4dcb9621c1d55650f
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-06 12:46:14 -0700

    [Studio] Modified screen "powewowsp2"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -3,16 +3,16 @@ function app($rootScope,$scope, app, $timeout, $ionicSideMenuDelegate) {
     'use strict';
     $ionicSideMenuDelegate.canDragContent(false);
     //$rootScope.bMenuVisible = false;
-    /*
+    /**/
     var timer = $timeout(function () {
-        console.warn("bMenuVisible="+$rootScope.bMenuVisible);
-        $rootScope.bMenuVisible = true; 
-        console.warn("bMenuVisible="+$rootScope.bMenuVisible);
-        //console.warn("entro");
+        //console.warn("bMenuVisible="+$rootScope.bMenuVisible);
+        //$rootScope.bMenuVisible = true; 
+        //console.warn("bMenuVisible="+$rootScope.bMenuVisible);
+        console.warn("entro");
         //$ionicSideMenuDelegate.canDragContent(true);
         
     }, 5000);
-    */
+    
     
     $rootScope.fEnableMenu = function(bValue){
          $rootScope.bMenuVisible = bValue;
