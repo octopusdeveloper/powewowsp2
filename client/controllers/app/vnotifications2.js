a92744cadc785be20de2acecc83c5f88ffd72283
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-07 09:37:32 -0700

    [Studio] Modified screen "vnotifications2"

--- client/controllers/app/vnotifications2.js
+++ client/controllers/app/vnotifications2.js
@@ -3,6 +3,12 @@ function app_vnotifications2($scope, app, $ionicHistory) {
     'use strict';
     app.init($scope);
     
+    
+    $scope.fBackWindow = function(){
+        console.warn("fBackWindow");
+        $ionicHistory.goBack();
+    }
+    
     $scope.fBackWindow = function(){
         console.warn("fBackWindow");
         $ionicHistory.goBack();
