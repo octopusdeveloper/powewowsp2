86452efb84592fb024056e54ecdf3997f50b40f9
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-07 10:15:48 -0700

    [Studio] Modified screen "vsettings"

--- client/controllers/app/vsettings.js
+++ client/controllers/app/vsettings.js
@@ -2,4 +2,7 @@ angular.module('app').controller('app_vsettings', app_vsettings);
 function app_vsettings($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $rootScope.configVersion="2.1.2";
+    $rootScope.configDescription="Whats new: Improved user experience and approval reminders";
 }
