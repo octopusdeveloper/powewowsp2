c365d0be6ab21420336b1770ce0b590a9a84b09b
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-07 12:48:12 -0700

    [Studio] Modified screen "vdetails"

--- client/controllers/app/vdetails.js
+++ client/controllers/app/vdetails.js
@@ -1,5 +1,10 @@
 angular.module('app').controller('app_vdetails', app_vdetails);
-function app_vdetails($scope, app) {
+function app_vdetails($scope, app, $ionicHistory) {
     'use strict';
     app.init($scope);
+    
+    $scope.fBackWindow = function(){
+        console.warn("fBackWindow");
+        $ionicHistory.goBack();
+    }
 }
