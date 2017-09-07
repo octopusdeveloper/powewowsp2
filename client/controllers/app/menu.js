ac6a61d463bf6eb29a1b9c295967765c1e6e8e4b
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-07 08:46:39 -0700

    [Studio] Modified screen "menu"

--- client/controllers/app/menu.js
+++ client/controllers/app/menu.js
@@ -2,4 +2,8 @@ angular.module('app').controller('app_menu', app_menu);
 function app_menu($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.fMenuGoTo = function (sValue){
+        app.go('app.');
+    }
 }
