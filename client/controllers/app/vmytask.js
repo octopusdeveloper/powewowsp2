039971a91e17b12c56594d2c9f993ac498fb9725
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-07 12:34:47 -0700

    [Studio] Modified screen "vmytask"

--- client/controllers/app/vmytask.js
+++ client/controllers/app/vmytask.js
@@ -2,4 +2,11 @@ angular.module('app').controller('app_vmytask', app_vmytask);
 function app_vmytask($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.fReject = function(){
+        console.warn("fReject");
+    }
+    $scope.fA = function(){
+        console.warn("fReject");
+    }
 }
