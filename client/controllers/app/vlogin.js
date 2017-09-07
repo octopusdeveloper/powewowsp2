64ee1c9011d4d858872671d0df7a7a79a1955a2d
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-07 14:23:46 -0700

    [Studio] Publish application, Modified screen "vLogin"

--- client/controllers/app/vlogin.js
+++ client/controllers/app/vlogin.js
@@ -6,7 +6,7 @@ function app_vlogin($scope, app, $timeout, $http) {
     $scope.sLoginUser = '';
     $scope.sLoginPass = '';
     $scope.sTest="nd"
-    $scope.fLogin2 = function (sLoginUser, sLoginPass) {
+    $scope.fLogin = function (sLoginUser, sLoginPass) {
         console.log('sUser=' + sLoginUser);
         console.log('sPass=' + sLoginPass);
         $scope.bLoading = true;
@@ -16,67 +16,6 @@ function app_vlogin($scope, app, $timeout, $http) {
         }, 2000);
     };
     
-    $scope.fLogin = function(sLoginUser, sLoginPass) {
-
-
-      var bActive = true;
-
-      console.log("sUser="+sLoginUser);
-      console.log("sPass="+sLoginPass);
-      
-
-
-      if (bActive){
-
-        
-        var paramsObj = new Object();
-        paramsObj.fduser = sLoginUser;
-        paramsObj.fdpassword = sLoginPass;
-
-        /* send post */
-
-        this.urlJson ="http://ebpm.octopusdeveloper.mockable.io/wsLogin";
-
-
-        $http({
-          method: 'POST',
-          url: this.urlJson ,
-          withCredentials: false,
-          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-          transformRequest: function(obj) {
-            var str = [];
-            for(var p in obj)
-              str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-            return str.join("&");
-          },
-          data: paramsObj
-        }).success(function (data, status) {
-          console.log("data="+JSON.stringify(data));
-          console.log("status="+JSON.stringify(status));
-          //console.log("resultAction="+JSON.stringify(data.result.resultAction));
-
-          if (data.result.resultAction){
-                app.go('app.vmytask');
- 
-          }else{
-            
-          }
-
-
-
-        }).error(function(data, status) {
-          //console.error('Repos error', status, data);
-
-          
-        });
-        /* end send post */
-
-
-
-      }
-
-
-
-  };
+   
 
 }
