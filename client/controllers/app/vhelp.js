6990bc2bfde707178a755c5ce3d68a92b08617e7
Jorge Carlos Ceciliano Esquinca <jorge.cecilianoesquinca@flex.com>
2017-09-07 09:31:39 -0700

    [Studio] Modified screen "vhelp"

--- client/controllers/app/vhelp.js
+++ client/controllers/app/vhelp.js
@@ -2,4 +2,12 @@ angular.module('app').controller('app_vhelp', app_vhelp);
 function app_vhelp($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.fCallHelpSVN = function(){
+        console.warn("fCallShareNet");    
+    }
+    
+    $scope.fCallShareNet = function(){
+        console.warn("fCallShareNet");    
+    }
 }
